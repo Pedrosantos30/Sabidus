b6248c789c8d9369160b348de7aff251df082a8f
Pedro Augusto <pedroaugustooficial17@gmail.com>
2024-11-23 17:00:35 -0300

    alterações no vercel

--- backend/src/App.js
+++ backend/src/App.js
@@ -6,7 +6,6 @@ require('dotenv').config();
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
 
 const dbURI = process.env.MONGO_URI;
@@ -18,8 +17,7 @@ mongoose.connect(dbURI)
 const usuarioRotas = require("./rotas/usuarioRotas");
 app.use("/usuarios", usuarioRotas);
 
-
-
- app.listen(5000, () => {
-  console.log("Servidor rodando na porta 5000!");
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}!`);
 });
